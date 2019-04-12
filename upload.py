f108b101403f1b45d70c18a25d6a5fc966fd9a18
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2019-04-12 11:47:16 +0200

    Add s3 write

--- upload.py
+++ upload.py
@@ -1,13 +1,16 @@
 import os
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
+import boto3
 
-UPLOAD_FOLDER = '/home/ubuntu'
+UPLOAD_FOLDER = '/tmp'
 
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+S3_BUCKET_NAME="zappa-lambda-bucket"
 
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 
@@ -20,20 +23,31 @@ def upload_form():
 
 @app.route('/', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-       if 'file' not in request.files:
-         flash('No file part')
-         return redirect(request.url)
-       file = request.files['file']
-       if file.filename == '':
-         flash('No file selected for uploading')
-         return redirect(request.url)
-       if file and allowed_file(file.filename):
-         filename = secure_filename(file.filename)
-         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-         flash('File(s) successfully uploaded')
-         return redirect('/')
+    try:
+        if request.method == 'POST':
+            # check if the post request has the file part
+           if 'file' not in request.files:
+             flash('No file part')
+             return redirect(request.url)
+           file = request.files['file']
+           if file.filename == '':
+             flash('No file selected for uploading')
+             return redirect(request.url)
+           if file and allowed_file(file.filename):
+             filename = secure_filename(file.filename)
+             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+             save_to_s3(open(os.path.join(app.config['UPLOAD_FOLDER'], filename)), S3_BUCKET_NAME, file.filename)
+             flash('File(s) successfully uploaded')
+             return redirect('/')
+    except Exception as e:
+        flash('Error in uploading file')
+        print e
+        return redirect('/')
+
+def save_to_s3(binary_data, bucket_name, key_name):
+    s3 = boto3.resource('s3')
+    object = s3.Object(bucket_name, key_name)
+    object.put(Body=binary_data)
 
 if __name__ == "__main__":
     app.run(port=8080, host='0.0.0.0')
