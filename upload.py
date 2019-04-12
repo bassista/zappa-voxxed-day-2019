67d8f63e8b16b45aeb9c83fbacf42ac49a034cea
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2019-04-12 16:37:16 +0200

    Parametrize function name

--- upload.py
+++ upload.py
@@ -21,7 +21,7 @@ def allowed_file(filename):
 
 @app.route('/')
 def upload_form():
-    return render_template('upload.html')
+    return render_template('upload.html', FUNC_NAME=FUNC_NAME)
 
 @app.route('/', methods=['POST'])
 def upload_file():
