a00ddaf64d8cb0ecff753658b9b871c9daa2d01f
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2019-04-12 12:06:53 +0200

    Add hello sample

---
+++ hello_voxxed.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello Voxxed people!", 200
+
+@app.route('/hello/<name>')
+@app.route('/hello/')
+def custom_hello(name=None):
+    if name is None:
+        return "Tell me your name", 200
+    else:
+        return "Welcome to voxxed day " + name + "!", 200
+
+# We only need this for local development.
+if __name__ == '__main__':
+    app.run()
