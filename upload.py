460cb32dc4f0321887fd5dbd5a8efbfec31dedf9
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2019-04-12 18:19:20 +0200

    Add gallery

--- upload.py
+++ upload.py
@@ -13,11 +13,34 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 S3_BUCKET_NAME="zappa-lambda-bucket-ireland"
+S3_BUCKET_NAME_THUMBNAIL="zappa-lambda-bucket-ireland-thumb"
 
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def save_to_s3(filename, bucket_name, key_name):
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file(filename, bucket_name, key_name)
+
+def resize(event, context):
+    client = boto3.client('s3')
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        local_path = '/tmp/' + key.split('/')[-1]
+        print "Download del file %s dal bucket %s" % (key, bucket_name)
+        client.download_file(bucket_name, key, local_path)
+        outfile = os.path.splitext(local_path)[0] + ".thumbnail"
+        try:
+            im = Image.open(local_path)
+            im.thumbnail(size)
+            im.save(outfile, "JPEG")
+            im.close()
+            save_to_s3(outfile, S3_BUCKET_NAME_THUMBNAIL, key)
+        except Exception as e:
+            print e
 
 @app.route('/')
 def upload_form():
@@ -40,18 +63,21 @@ def upload_file():
              print "ok"
              filename = secure_filename(file.filename)
              file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-             save_to_s3(open(os.path.join(app.config['UPLOAD_FOLDER'], filename)), S3_BUCKET_NAME, file.filename)
+             save_to_s3(os.path.join(app.config['UPLOAD_FOLDER'], filename), S3_BUCKET_NAME, file.filename)
              flash('File(s) successfully uploaded')
              return redirect('/upload')
     except Exception as e:
         flash('Error in uploading file')
         print e
-        return redirect('/')
+        return redirect('/upload')
 
-def save_to_s3(binary_data, bucket_name, key_name):
-    s3 = boto3.resource('s3')
-    object = s3.Object(bucket_name, key_name)
-    object.put(Body=binary_data)
+@app.route('/gallery/')
+@app.route('/gallery')
+def custom_hello(name=None):
+    conn = client('s3')
+    for key in conn.list_objects(Bucket=S3_BUCKET_NAME)['Contents']:
+        print(key['Key'])
+    return render_template('gallery.html')
 
 if __name__ == "__main__":
     app.run(port=8080, host='0.0.0.0')
